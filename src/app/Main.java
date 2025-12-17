b19d775a385246f1dd5d5c926cfce16e12113578
Polischyk_Oleksandr <sashap07072010@gmail.com>
2025-12-17 18:51:20 +0200

    Init commit

---
+++ src/app/Main.java
@@ -0,0 +1,11 @@
+package app;
+
+/**
+ * author: user,
+ * date: 17.12.2025
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Project with git connect (SSH)\nMy project");
+    }
+}
