efbae8633fbe1d3db037708a718d3f232e709953
Polischyk_Oleksandr <sashap07072010@gmail.com>
2025-12-17 19:02:42 +0200

    Added converting miles to kilometers and in reverse

--- src/app/Main.java
+++ src/app/Main.java
@@ -5,7 +5,23 @@ package app;
  * date: 17.12.2025
  */
 public class Main {
+    private static final double MILES_CONST = 1.60934;
+
     public static void main(String[] args) {
         System.out.println("Project with git connect (SSH)\nMy project");
+        System.out.println("App for kilometres to miles converting.");
+        System.out.println("Version 1.0.");
+        double kilometers = 5;
+        double miles = 12;
+        double milesInKilometers = convKilometersToMiles(kilometers);
+        double kilometersInMiles = convMilesToKilometers(miles);
+        System.out.println("Result is " + milesInKilometers + " milesInKilometers and "
+                + kilometersInMiles + " kilometersInMiles.");
+    }
+    private static double convKilometersToMiles(double kilometers){
+        return kilometers / MILES_CONST;
+    }
+    private static double convMilesToKilometers(double miles){
+        return miles * MILES_CONST;
     }
 }
